59c70772ac5c41c3278d03bc1eb585e1d47d4473
NghiVH <nghivh@unifiedpost.com>
2024-03-05 20:54:51 +0700

    [first commit]

---
+++ src/test/java/Home/chrome.java
@@ -0,0 +1,14 @@
+package Home;
+
+import org.testng.annotations.Test;
+
+public class chrome {
+
+    @Test
+    public void chrome1() {
+
+        System.out.println(System.getProperty("browser"));
+        System.out.println("Chrome browser has been run .....");
+    }
+
+}
---
+++ src/test/java/Home/firefox.java
@@ -0,0 +1,13 @@
+package Home;
+
+import org.testng.annotations.Test;
+
+public class firefox {
+
+    @Test
+    public void firefox1() {
+
+        System.out.println(System.getProperty("browser"));
+        System.out.println("ff browser has been run .....");
+    }
+}
